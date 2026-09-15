40b2bccb4a5e4289780c6d04ec87c97bbe7b16b0
SunnyShekhar-code <sunnyshekhar1729@gmail.com>
2026-09-15 21:47:07 +0530

    persist() versus merge() save() versus JPA dirty checking Derived query methods Query keywords such as Containing, Between, Top and OrderBy

--- src/main/java/in/strikes/jpaRelationShipDemo/repository/StudentRepository.java
+++ src/main/java/in/strikes/jpaRelationShipDemo/repository/StudentRepository.java
@@ -2,20 +2,26 @@ package in.strikes.jpaRelationShipDemo.repository;
 
 import java.util.Optional;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import in.strikes.jpaRelationShipDemo.model.Student;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+
+
 
 
 public interface StudentRepository extends JpaRepository<Student,Long>{
 
     // Student save(Student student);
     // Optional<Student> findById(Long id);
+
+    @Query(value = """
+            select * from Student where email=?1 and age=?2
+            """)
+    Optional<Student> findByEmailAndage(String email, Long age);
+    // Optional<Student> findByEmailAndage(@Param("email") String email, @Param("age") Long age);
 
     
 }
